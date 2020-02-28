86988234db81b872a18e1491b1250e454cc4369e
Blake Noll <nollblake@gmail.com>
2020-02-28 14:01:18 -0800

    mongo connection fix

--- api/utils/mongoClient.js
+++ api/utils/mongoClient.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 const uri =
-  "mongodb+srv://mongo:mongoose@cluster0-og5eu.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+  "mongodb+srv://mongoose:mongoose@cluster0-og5eu.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 module.exports = client;
